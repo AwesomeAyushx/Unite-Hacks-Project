79d0e0e395c8ca9acdadbc764fb15d959cfb4179
Gamelander4 <praneel.nemani@gmail.com>
2022-10-22 15:01:17 -0700

    deleted unecessary

---
+++ backend/TaskWorth File.cpp
@@ -0,0 +1,13 @@
+/*#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int category_amount, worth_in;
+    double worth_edited;
+    vector <double> worths;
+    for (int i = 0; i < category_amount; i++) {
+        cin >> worth_in;
+        worth_edited = worth_in/100;
+        worths.push_back(worth_edited);
+    }
+}*/
