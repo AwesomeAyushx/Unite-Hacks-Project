71589964cf3b1dcc2a7543c1a2d3bdb950c85c8d
AwesomeAyushx <74789190+AwesomeAyushx@users.noreply.github.com>
2022-10-22 16:14:15 -0700

    e

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express')
+const fs =require('fs')
 const app = express()
 const port = 3000
 app.use(express.static('public'))
+/*fs.readFileSync('communications.txt', (err, data) => {
+  if (err) throw err;
 
+  console.log(data.toString());
+})*/
+data = {
+  classes:{
+    class1: {
+      type : "honors",
+      grade: "C"
+    },
+    class2: {
+      type : "AP",
+      grade: "A"
+    },
+  }
+}
+/*
+fs.writeFile('communications.txt', JSON.stringify(data), (err) => {
+  if (err) throw err;
+})*/
+console.log(JSON.stringify(data))
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
