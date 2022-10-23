17272974710ec30852e0bcb8b84d6a5d5848cdd4
Gamelander4 <praneel.nemani@gmail.com>
2022-10-22 19:06:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -64,19 +64,8 @@ function gpa(classes){
     GPA = total_points/classes;
     return GPA
   }
-  function gradeCalc(list1, list2){
-    var complete_total = 0, average_total = 0, total = 0;
-    const categoryworth = list1;
-    for (var i = 0; i < categoryworth.length; i++) {
-        var average = 0;
-        const assignments = list2   ;
-        for (var j = 0; j < assignments.length; j++) {
-            total += assignments[j];
-        }
-        average = (total/assignments.length) * categoryworth[i];
-        complete_total += average;
-    }
-    average_total = (complete_total)*50;
+  function gradeCalc(point_earned, points_total){
+    var average_total = (point_earned/points_total)*100
     if (average_total >= 90) {
         return ("A")
     }
